d330a591b62abf9733664d94cdc3fcbff0600c69
barthy <b.bonhomme@vucx.de>
2023-02-11 16:26:43 +0100

    [IMPROVEMENT] tsc and version

---
+++ gh-pages/demo/slider-responsive.js
@@ -0,0 +1,23 @@
+import { ScrollSnapSlider } from '../dist/scroll-snap-slider.esm.js'
+
+const sliderResponsiveElement = document.querySelector('.scroll-snap-slider.-responsive')
+const sliderResponsive = new ScrollSnapSlider({ element: sliderResponsiveElement })
+
+const arrows = document.querySelector('.indicators.-responsive')
+const prev = document.querySelector('.indicators.-responsive .arrow.-prev')
+const next = document.querySelector('.indicators.-responsive .arrow.-next')
+
+const updateArrows = function () {
+  arrows.classList.toggle('-hidden', sliderResponsiveElement.scrollWidth <= sliderResponsiveElement.clientWidth)
+}
+
+prev.addEventListener('click', function () {
+  sliderResponsive.slideTo(sliderResponsive.slide - 1)
+})
+
+next.addEventListener('click', function () {
+  sliderResponsive.slideTo(sliderResponsive.slide + 1)
+})
+
+window.addEventListener('resize', updateArrows)
+updateArrows()
