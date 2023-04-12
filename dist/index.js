6c830642550bc7c8c2d3643df1117b002e1bfb17
barthy <post@barthy.koeln>
2023-04-12 09:55:01 +0200

    vite UMD and ESM build

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { ScrollSnapAutoplay } from './ScrollSnapAutoplay';
-import { ScrollSnapDraggable } from './ScrollSnapDraggable';
-import { ScrollSnapLoop } from './ScrollSnapLoop';
-import { ScrollSnapPlugin } from './ScrollSnapPlugin';
-import { ScrollSnapSlider } from './ScrollSnapSlider';
-export { ScrollSnapSlider, ScrollSnapPlugin, ScrollSnapLoop, ScrollSnapAutoplay, ScrollSnapDraggable };
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    dts()
+  ],
+  build: {
+    target: 'ESNext',
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'scroll-snap-slider'
+    }
+  }
+})
